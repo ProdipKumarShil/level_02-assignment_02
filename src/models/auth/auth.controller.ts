1ce50353f65c929705c73f7b45117f4986413677
ProdipKumarShil <prodipkrishna01@gmail.com>
2026-05-30 23:31:16 +0600

    added user sign up

--- src/models/auth/auth.controller.ts
+++ src/models/auth/auth.controller.ts
@@ -1,8 +1,33 @@
-const userSignup = async() => {
-  
+import type { Request, Response } from "express"
+import { authService } from "./auth.service"
+import { StatusCodes } from "http-status-codes"
+
+const userSignup = async (req: Request, res: Response) => {
+  try {
+    const result = await authService.createUserIntoDB(req.body)
+
+    res.status(StatusCodes.CREATED).json({
+      success: true,
+      message: "User registered successfully",
+      data: result.rows[0]
+    })
+
+  } catch (error) {
+    res.status(500).send({
+      status: false,
+      message: error.message
+    })
+  }
 }
-const userLogin = async() => {
-  
+const userLogin = async (req: Request, res: Response) => {
+  try {
+    console.log(req.body)
+  } catch (error) {
+    res.status(500).send({
+      status: false,
+      message: error.message
+    })
+  }
 }
 
-export const authController = {userSignup, userLogin}
+export const authController = { userSignup, userLogin }
--- src/models/auth/auth.route.ts
+++ src/models/auth/auth.route.ts
@@ -4,7 +4,7 @@ import { authController } from "./auth.controller";
 
 const router = Router()
 
-app.post('/signup', authController.userSignup)
-app.post('/login', authController.userLogin)
+router.post('/signup', authController.userSignup)
+router.post('/login', authController.userLogin)
 
 export const authRoute = router
